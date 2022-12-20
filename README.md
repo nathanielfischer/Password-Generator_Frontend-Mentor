f1a3126d273eeb85010930bac8c8ba77be77097f
Nathaniel Fischer <nathaniel.f@web.de>
2022-12-20 16:56:54 +0100

    Created README.md and updated .gitignore

---
+++ README.md
@@ -0,0 +1,52 @@
+# Frontend Mentor - Password generator app solution
+
+This is a solution to the [Password generator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/password-generator-app-Mr8CLycqjh). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- Generate a password based on the selected inclusion options
+- Copy the generated password to the computer's clipboard
+- See a strength rating for their generated password
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+
+### Screenshot
+
+![](./docs/screenshot.png)
+
+### Links
+
+- Solution URL: [Solution on Github Pages](https://nathanielfischer.github.io/GitHub-User-Search_Frontend-Mentor/)
+
+## My process
+
+### Built with
+
+- HTML5
+- CSS
+- Bootstrap
+- JavaScript
+- jQuery
+
+### What I learned
+
+I've got a better understanding of how to navigate the DOM with jQuery.
+
+## Author
+
+- Github - [Nathaniel Fischer](https://github.com/nathanielfischer)
--- index.js
+++ index.js
@@ -13,8 +13,8 @@ $("button").click(function(event) {
 
 
 $(document).on("input", ".form-range", function() {
-    $(".character").text($(".form-range")[0].value);
-    console.log($(".form-range")[0].value);
+  $(".character").text($(".form-range")[0].value);
+  console.log($(".form-range")[0].value);
 });
 
 
@@ -23,7 +23,7 @@ $(".fa-copy").click(function(event) {
   var copyText = $(".password").text();
   navigator.clipboard.writeText(copyText);
   $(".fa-copy").fadeOut(80);
-  setTimeout(function(){
+  setTimeout(function() {
     $(".fa-copy").fadeIn(80);
   }, 80);
 });
@@ -32,38 +32,38 @@ $(".fa-copy").click(function(event) {
 // ----------------------- generate Password -----------------------
 
 function generatePassword(length) {
-    var result = "";
-    var characters = "";
+  var result = "";
+  var characters = "";
 
 
-    if($("#defaultCheck1")[0].checked){
-      characters += upperCaseLetters;
-    }
-    if($("#defaultCheck2")[0].checked){
-      characters += lowerCaseLetters;
-    }
-    if($("#defaultCheck3")[0].checked){
-      characters += numbers;
-    }
-    if($("#defaultCheck4")[0].checked){
-      characters += symbols;
-    }
+  if ($("#defaultCheck1")[0].checked) {
+    characters += upperCaseLetters;
+  }
+  if ($("#defaultCheck2")[0].checked) {
+    characters += lowerCaseLetters;
+  }
+  if ($("#defaultCheck3")[0].checked) {
+    characters += numbers;
+  }
+  if ($("#defaultCheck4")[0].checked) {
+    characters += symbols;
+  }
 
-    var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
 
-    strengthCalculator(result);
+  strengthCalculator(result);
 
-    $(".password").text(result);
-    $(".password").addClass("password-set");
+  $(".password").text(result);
+  $(".password").addClass("password-set");
 }
 
 
 
 
-function strengthCalculator(password){
+function strengthCalculator(password) {
   var lowerCount = 0;
   var upperCount = 0;
   var numbersCount = 0;
@@ -92,12 +92,12 @@ function strengthCalculator(password){
   var strength = "WEAK"; // 1 = weak, 2 = medium, 3 = strong, 4 = very strong
   var charsSafety = lowerCount + upperCount + numbersCount + symbolsCount;
 
-  if (pwLength >=5 && pwLength <= 7) {
+  if (pwLength >= 5 && pwLength <= 7) {
     strength = "MEDIUM";
-  }else if (pwLength >=8 && pwLength <= 10) {
-    strength ="STRONG";
-  }else if (pwLength >= 11) {
-    strength ="VERY STRONG";
+  } else if (pwLength >= 8 && pwLength <= 10) {
+    strength = "STRONG";
+  } else if (pwLength >= 11) {
+    strength = "VERY STRONG";
   }
 
   $(".strength-keyword").text(strength);
